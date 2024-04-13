6367b31c1b3c4f246aedea0734dbda600b340fd5
Luzin7 <lvictor.dutra@gmail.com>
2024-04-13 14:16:27 -0300

    feat: add dropdown menu

--- src/components/Header/components/UserIcon/UserIcon.tsx
+++ src/components/Header/components/UserIcon/UserIcon.tsx
@@ -3,22 +3,34 @@
 import { useUserStore } from "@/store/user";
 import { localPaths } from "@/utils/localPaths";
 import Link from "next/link";
+import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import styles from "./userIcon.module.css";
 
 export function UserIcon() {
   const { userState } = useUserStore();
+  const [isDropdownActive, setIsDropdownActive] = useState<boolean>(false);
 
   return (
     <>
       {userState.user.isLoggedIn ? (
         <div
           className={styles.icon_container}
-          style={{
-            cursor: "pointer",
-          }}
+          onClick={() => setIsDropdownActive(!isDropdownActive)}
+          onBlur={() => setIsDropdownActive(false)}
         >
           <FaUser />
+          <div
+            className={styles.dropdown}
+            id={isDropdownActive ? styles.dropdown_active : ""}
+          >
+            <Link href={localPaths.RECOMMENDATION} className={styles.icon_link}>
+              Recomendar
+            </Link>
+            <Link href={localPaths.HOME} className={styles.icon_link}>
+              Sair
+            </Link>
+          </div>
         </div>
       ) : (
         <Link href={localPaths.SIGNIN}>
