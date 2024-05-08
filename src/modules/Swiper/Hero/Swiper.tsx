397a61e3002e51feaad1274e5e690a4eaf1eefd4
Luzin7 <lvictor.dutra@gmail.com>
2024-05-08 19:01:21 -0300

    fix: fix prerender error

--- src/modules/Swiper/Hero/Swiper.tsx
+++ src/modules/Swiper/Hero/Swiper.tsx
@@ -13,70 +13,54 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./swiper.module.css";
 
 export function HeroSwiper({ banners }: HeroSwiperProps) {
-  if (typeof window !== "undefined") {
-    const getSwiperWidth = () => {
-      const windowWidth = window.innerWidth;
-      const mobile = 390;
-      const desktop = 1920;
-
-      if (windowWidth >= desktop) {
-        return "1800px";
-      } else if (windowWidth <= mobile) {
-        return "100vw";
-      }
-
-      return "100vw";
-    };
-
-    return (
-      <section>
-        <div className={styles.mask}></div>
-        <Swiper
-          slidesPerView={"auto"}
-          loop={true}
-          autoplay={{
-            delay: 7000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-            type: "progressbar",
-          }}
-          modules={[Autoplay, Pagination]}
-        >
-          {banners.map(({ id, banner, shortDescription, title }) => (
-            <SwiperSlide
-              key={id}
-              style={{
-                width: getSwiperWidth(),
-                height: "500px",
-                marginLeft: "40px",
-              }}
-            >
-              <div className={styles.banner_content__container}>
-                <div
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                  }}
-                >
-                  <h2>{title}</h2>
-                  <p>{shortDescription}</p>
-                  <Link href={`movie/${urlSlugMaker(title)}/${id}`}>
-                    Saiba mais
-                  </Link>
-                </div>
-                <Image
-                  fill
-                  key={id}
-                  src={`https://image.tmdb.org/t/p/original${banner}`}
-                  alt={title}
-                />
+  return (
+    <section>
+      <div className={styles.mask}></div>
+      <Swiper
+        slidesPerView={"auto"}
+        loop={true}
+        autoplay={{
+          delay: 7000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          type: "progressbar",
+        }}
+        modules={[Autoplay, Pagination]}
+      >
+        {banners.map(({ id, banner, shortDescription, title }) => (
+          <SwiperSlide
+            key={id}
+            style={{
+              width: "100vw",
+              height: "500px",
+              marginLeft: "40px",
+            }}
+          >
+            <div className={styles.banner_content__container}>
+              <div
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
+              >
+                <h2>{title}</h2>
+                <p>{shortDescription}</p>
+                <Link href={`movie/${urlSlugMaker(title)}/${id}`}>
+                  Saiba mais
+                </Link>
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </section>
-    );
-  }
+              <Image
+                fill
+                key={id}
+                src={`https://image.tmdb.org/t/p/original${banner}`}
+                alt={title}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 }
