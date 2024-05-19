dcfcec3c7f0169b5632d8bad2455829a867f21e7
Luzin7 <lvictor.dutra@gmail.com>
2024-05-19 20:37:34 -0300

    bomb commit for dev

--- src/components/Admin/components/AdminSearchForm/AdminSearchForm.tsx
+++ src/components/Admin/components/AdminSearchForm/AdminSearchForm.tsx
@@ -1,53 +1,53 @@
-'use client'
+// 'use client'
 
-import { Form } from '@components/Form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useLoading } from '@hooks/useLoading'
-import { searchMovie, searchMovieProps } from '@schemas/search/querySchema'
-import { fetchMoviesByQuery } from '@services/tmdbApi'
-import { useRecommendationStore } from '@store/recommendation'
-import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
-import styles from './adminSearchForm.module.css'
+// import { Form } from '@components/Form'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useLoading } from '@hooks/useLoading'
+// import { searchMovie, searchMovieProps } from '@schemas/search/querySchema'
+// import { fetchMoviesByQuery } from '@services/tmdbApi'
+// import { useRecommendationStore } from '@store/recommendation'
+// import { useForm } from 'react-hook-form'
+// import { toast } from 'react-toastify'
+// import styles from './adminSearchForm.module.css'
 
-export function AdminSearchForm() {
-  const { setIsLoading } = useLoading()
+// export function AdminSearchForm() {
+//   const { setIsLoading } = useLoading()
 
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<searchMovieProps>({
-    resolver: zodResolver(searchMovie),
-  })
+//   const {
+//     handleSubmit,
+//     register,
+//     formState: { errors },
+//   } = useForm<searchMovieProps>({
+//     resolver: zodResolver(searchMovie),
+//   })
 
-  const onSubmit = async ({ query }: searchMovieProps) => {
-    setIsLoading((prevState) => !prevState)
-    try {
-      const response = await fetchMoviesByQuery(query)
+//   const onSubmit = async ({ query }: searchMovieProps) => {
+//     setIsLoading((prevState) => !prevState)
+//     try {
+//       const response = await fetchMoviesByQuery(query)
 
-      useRecommendationStore.setState({ content: response })
+//       useRecommendationStore.setState({ content: response })
 
-      setIsLoading((prevState) => !prevState)
-    } catch (error) {
-      toast.error('Deu em arrobas, papai')
-      setIsLoading((prevState) => !prevState)
-    }
-  }
+//       setIsLoading((prevState) => !prevState)
+//     } catch (error) {
+//       toast.error('Deu em arrobas, papai')
+//       setIsLoading((prevState) => !prevState)
+//     }
+//   }
 
-  return (
-    <Form.Wrapper onSubmit={handleSubmit(onSubmit)}>
-      <div className={styles.container}>
-        <h2>Fala, meu pexe!</h2>
-        <Form.Input
-          type="text"
-          placeholder="Nome do filme"
-          {...register('query')}
-        />
-        {errors.query?.message !== undefined && (
-          <Form.Error errorMessage={errors.query.message} />
-        )}
-      </div>
-    </Form.Wrapper>
-  )
-}
+//   return (
+//     <Form.Wrapper onSubmit={handleSubmit(onSubmit)}>
+//       <div className={styles.container}>
+//         <h2>Fala, meu pexe!</h2>
+//         <Form.Input
+//           type="text"
+//           placeholder="Nome do filme"
+//           {...register('query')}
+//         />
+//         {errors.query?.message !== undefined && (
+//           <Form.Error errorMessage={errors.query.message} />
+//         )}
+//       </div>
+//     </Form.Wrapper>
+//   )
+// }
--- src/components/Admin/components/RecommendationModal/RecommendationModal.tsx
+++ src/components/Admin/components/RecommendationModal/RecommendationModal.tsx
@@ -1,97 +1,97 @@
-'use client'
+// 'use client'
 
-import { Form } from '@components/Form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useLoading } from '@hooks/useLoading'
-import { TmdbGeneralContentProps } from '@models/tmdb'
-import { addContent, addContentProps } from '@schemas/admin/addContent'
-import { addMovie } from '@services/cenaestelarApi'
-import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
-import styles from '../../../../styles/modal.module.css'
+// import { Form } from '@components/Form'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useLoading } from '@hooks/useLoading'
+// import { TmdbGeneralContentProps } from '@models/tmdb'
+// import { addContent, addContentProps } from '@schemas/admin/addContent'
+// import { addMovie } from '@services/cenaestelarApi'
+// import { useForm } from 'react-hook-form'
+// import { toast } from 'react-toastify'
+// import styles from '../../../../styles/modal.module.css'
 
-interface ModalProps {
-  isOpen: boolean
-  setIsOpen: (isOpen: boolean) => void
-  title: string
-  data: TmdbGeneralContentProps
-}
+// interface ModalProps {
+//   isOpen: boolean
+//   setIsOpen: (isOpen: boolean) => void
+//   title: string
+//   data: TmdbGeneralContentProps
+// }
 
-export function RecommendationModal({
-  data,
-  isOpen,
-  setIsOpen,
-  title,
-}: ModalProps) {
-  const { isLoading, setIsLoading } = useLoading()
+// export function RecommendationModal({
+//   data,
+//   isOpen,
+//   setIsOpen,
+//   title,
+// }: ModalProps) {
+//   const { isLoading, setIsLoading } = useLoading()
 
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<addContentProps>({
-    resolver: zodResolver(addContent),
-  })
+//   const {
+//     handleSubmit,
+//     register,
+//     formState: { errors },
+//   } = useForm<addContentProps>({
+//     resolver: zodResolver(addContent),
+//   })
 
-  const handleRecommend = async ({
-    description,
-    shortDescription,
-    rating,
-  }: addContentProps) => {
-    setIsLoading((prevState) => !prevState)
+//   const handleRecommend = async ({
+//     description,
+//     shortDescription,
+//     rating,
+//   }: addContentProps) => {
+//     setIsLoading((prevState) => !prevState)
 
-    try {
-      await addMovie(data.id, shortDescription, description, rating)
-      setIsLoading((prevState) => !prevState)
-      toast('Filme adicionado ao cena estelar!')
-      setIsOpen(!isOpen)
-    } catch (error) {
-      setIsLoading((prevState) => !prevState)
-      toast.error('Não foi possível adicionar o conteúdo ao Cena Estelar!')
-      setIsOpen(!isOpen)
-    }
-  }
+//     try {
+//       await addMovie(data.id, shortDescription, description, rating)
+//       setIsLoading((prevState) => !prevState)
+//       toast('Filme adicionado ao cena estelar!')
+//       setIsOpen(!isOpen)
+//     } catch (error) {
+//       setIsLoading((prevState) => !prevState)
+//       toast.error('Não foi possível adicionar o conteúdo ao Cena Estelar!')
+//       setIsOpen(!isOpen)
+//     }
+//   }
 
-  if (isOpen) {
-    return (
-      <div className={styles.container}>
-        <div className={styles.content_wrapper}>
-          <Form.Wrapper onSubmit={handleSubmit(handleRecommend)}>
-            <h2>{title}</h2>
-            <Form.Input
-              type="text"
-              placeholder="Nota da bomba"
-              {...register('rating')}
-            />
-            {errors.rating?.message !== undefined && (
-              <Form.Error errorMessage={errors.rating.message} />
-            )}
-            <Form.Input
-              type="text"
-              placeholder="Descrição curta da parada"
-              {...register('shortDescription')}
-            />
-            {errors.shortDescription?.message !== undefined && (
-              <Form.Error errorMessage={errors.shortDescription.message} />
-            )}
-            <textarea
-              style={{ color: 'black' }}
-              placeholder="Descrição poggers"
-              {...register('description')}
-              cols={70}
-              rows={4}
-            />
-            {errors.description?.message !== undefined && (
-              <Form.Error errorMessage={errors.description.message} />
-            )}
-            <Form.Button type="submit">
-              {isLoading ? 'AUUUUU' : 'Xama!'}
-            </Form.Button>
-          </Form.Wrapper>
-        </div>
-      </div>
-    )
-  } else {
-    return <></>
-  }
-}
+//   if (isOpen) {
+//     return (
+//       <div className={styles.container}>
+//         <div className={styles.content_wrapper}>
+//           <Form.Wrapper onSubmit={handleSubmit(handleRecommend)}>
+//             <h2>{title}</h2>
+//             <Form.Input
+//               type="text"
+//               placeholder="Nota da bomba"
+//               {...register('rating')}
+//             />
+//             {errors.rating?.message !== undefined && (
+//               <Form.Error errorMessage={errors.rating.message} />
+//             )}
+//             <Form.Input
+//               type="text"
+//               placeholder="Descrição curta da parada"
+//               {...register('shortDescription')}
+//             />
+//             {errors.shortDescription?.message !== undefined && (
+//               <Form.Error errorMessage={errors.shortDescription.message} />
+//             )}
+//             <textarea
+//               style={{ color: 'black' }}
+//               placeholder="Descrição poggers"
+//               {...register('description')}
+//               cols={70}
+//               rows={4}
+//             />
+//             {errors.description?.message !== undefined && (
+//               <Form.Error errorMessage={errors.description.message} />
+//             )}
+//             <Form.Button type="submit">
+//               {isLoading ? 'AUUUUU' : 'Xama!'}
+//             </Form.Button>
+//           </Form.Wrapper>
+//         </div>
+//       </div>
+//     )
+//   } else {
+//     return <></>
+//   }
+// }
